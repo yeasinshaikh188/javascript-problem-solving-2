f24a6631d1d0f4eab5915bdcc1e0a31db3dc5096
yeasinshaikh188@gmail.com <yeasinshaikh188@gmail.com>
2024-05-31 20:55:34 +0600

    creat a name.js file for assignmetn 2

---
+++ name.js
@@ -0,0 +1,27 @@
+// ans no 1 ---------------
+function celsiusToFahrenheit(celsius) {
+    let celNum = celsius;
+    let result = celNum * 9 / 5 + 32;
+    return result;
+
+  }
+
+  console.log(celsiusToFahrenheit(0)); // 32
+  console.log(celsiusToFahrenheit(25)); // 77
+
+// ans no 2 ----------------
+
+function isEven(num) {
+   if(num % 2 === 0){
+    return true;
+   }else{
+    return false;
+   }
+  }
+  
+  console.log(isEven(4)); // true
+  console.log(isEven(7)); // false
+  
+  // ans no 3 ----------------
+  
+  
